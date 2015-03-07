20c4f0f52a9815aef6958b0736819b733ef14fd1
Harry Choi <chharry@gmail.com>
2015-03-07 15:25:23 +0900

    Added local settings

---
+++ dowant/local_settings.py
@@ -0,0 +1,10 @@
+ADMINS = (
+  ('guestpub', 'pointer81@gmail.com'),
+  )
+
+EMAIL_HOST = 'smtp.cafe24.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587 
+EMAIL_HOST_USER = 'hoodpub@hoodpub.com'
+EMAIL_HOST_PASSWORD = 'hmchoi81' 
+SERVER_EMAIL = 'pointer81@gmail.com'
