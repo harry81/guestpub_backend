364327e00f1cf6991bc70d3669fe547ec8647c54
Harry Choi <chharry@gmail.com>
2015-03-08 17:25:01 +0900

    Use function view instead of class view to avoid csrf problem

--- guestpub/urls.py
+++ guestpub/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, url
-from guestpub.views import PubList, PubDetail, MessageList, MessageDetail
+from guestpub.views import PubList, PubDetail, message_list
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('guestpub.views',
                     url(r'^pub/$', PubList.as_view()),
                     url(r'^pub/(?P<pk>[0-9]+)/$', PubDetail.as_view()),
-                    url(r'^message$', MessageList.as_view()),
-                    url(r'^message/(?P<pk>[0-9]+)/$', MessageDetail.as_view()),
+                    url(r'^message$', message_list),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- guestpub/views.py
+++ guestpub/views.py
@@ -1,9 +1,9 @@
-import json
-from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
-from guestpub.serializers import PubSerializer, MessageSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework import generics
+from guestpub.serializers import PubSerializer, MessageSerializer
 from guestpub.models import Pub, Message
 
 
@@ -24,9 +24,17 @@ class MessageList(generics.ListCreateAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
+@api_view(['GET', 'POST'])
+def message_list(request):
+    if request.method == 'GET':
+        snippets = Message.objects.all()
+        serializer = MessageSerializer(snippets, many=True)
+        return Response(serializer.data)
 
-class MessageDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = ()
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
+    elif request.method == 'POST':
+        serializer = MessageSerializer(data=request.POST)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
