c05312d0d9dd99a8680d22839b6fb978faa86d4f
Hyunmin Choi <pointer81@gmail.com>
2015-03-15 16:12:32 +0900

    Added api test cases

--- guestpub/tests.py
+++ guestpub/tests.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.test import Client
 from mock import patch, Mock
 import urllib2
 from helpers import MessageGateway, get_comment
@@ -17,16 +18,30 @@ class PubScrapyTest(TestCase):
         mock_urlopen.return_value = mock
 
         response = get_comment('12026394')
-
         self.assertEqual(response['homepage'], 'http://cafe.naver.com/jejusai', 'Homepage is different')
         self.assertEqual(len(response['comments']), 6)
         self.assertEqual(response['comments'][3]['time'], '2012-04-01')
 
-
-class GuestpubModelTest(TestCase):
+class APITest(TestCase):
     fixtures = ['initial_data']
 
-    def test_get_comment(self):
+    def test_pub_list(self):
+        c = Client()
+        response = c.get('/api/pub/')
+        self.assertTrue('13080037' in response.content)
+
+    def test_pub_detail(self):
+        c = Client()
+        response = c.get('/api/pub/13080037/')
+        self.assertTrue('13080037' in response.content)
+
+
+        
+
+class ModelTest(TestCase):
+    fixtures = ['initial_data']
+
+    def test_get_comment_when_save(self):
         pub = Pub.objects.all()[0]
         pub._get_comment()
         self.assertEqual(Pub.objects.all().count(), 15, u'숙소의 개수가 15개 아님.')
